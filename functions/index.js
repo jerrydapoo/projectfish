f54d6c75316f580a163f4a7791b9692ca7cd8f2b
jerrydapoo <hendrix.chino@gmail.com>
2024-10-30 17:13:24 +0800

    dghryrtdy

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,23 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const bodyParser = require('body-parser');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 admin.initializeApp();
 const db = admin.firestore();
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/submit-login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        await db.collection('users').add({ username, password });
-        res.send('User data saved successfully.');
-    } catch (error) {
-        console.error('Error saving data:', error);
-        res.status(500).send('Error saving data: ' + error);
-    }
+app.post("/submit-login", async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    await db.collection("users").add({username, password});
+    res.send("User data saved successfully.");
+  } catch (error) {
+    console.error("Error saving data:", error);
+    res.status(500).send("Error saving data: " + error);
+  }
 });
 
 exports.app = functions.https.onRequest(app);
